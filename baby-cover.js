fd9762951e6aeb34eb513a0372fbc157bb14e79e
archfndr <dudgh2609@gmail.com>
2026-09-07 22:09:54 +0900

    Override Dijon Baby cover with 3000px artwork

---
+++ baby-cover.js
@@ -0,0 +1,2 @@
+window.ARCHFNDR_MEDIA = window.ARCHFNDR_MEDIA || {};
+window.ARCHFNDR_MEDIA.baby = 'https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/b0/0b/d3/b00bd346-7b14-da61-28b0-94d2ea03f58e/093624826873.jpg/3000x3000bb.jpg';
